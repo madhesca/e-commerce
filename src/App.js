0a97b94f8693264ca4aa9361e6790f601ce7c01d
marton <mpochavillo@gmail.com>
2020-05-16 22:31:30 +0800

    renewed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from "./pages/homepage/homepage.component";
+
+const Hats = () => {
+  return <h1>Hats</h1>;
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={Hats} />
+      </Switch>
     </div>
   );
 }
